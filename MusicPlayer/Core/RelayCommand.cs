c11ce95d3d4b797373a3fe7a6300f1c8a11a4f87
JrnJ <jeroenjochems@outlook.com>
2022-04-03 22:30:27 +0200

    Latest non-MVVM code

---
+++ MusicPlayer/Core/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MusicPlayer.Core
+{
+    class RelayCommand : ICommand
+    {
+        // 
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        // Constructor
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        // Commands
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
--- MusicPlayer/MVVM/ViewModel/MainViewModel.cs
+++ MusicPlayer/MVVM/ViewModel/MainViewModel.cs
@@ -5,13 +5,22 @@ namespace MusicPlayer.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+        // Commands
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand PlaylistViewCommand { get; set; }
+
+        // ViewModels
+        public HomeViewModel HomeVM { get; set; }
+
         public PlaylistViewModel PlaylistVM { get; set; }
 
+        // Current View
         private object _currentView;
 
         public object CurrentView
         {
-            get => _currentView;
+            get { return _currentView; }
             set
             {
                 _currentView = value;
@@ -19,10 +28,26 @@ namespace MusicPlayer.MVVM.ViewModel
             }
         }
 
-
+        // Constructor
         public MainViewModel()
         {
+            // Create ViewModels
+            HomeVM = new HomeViewModel();
             PlaylistVM = new PlaylistViewModel();
+            
+            // Set a default
+            CurrentView = HomeVM;
+
+            // Assign Commands
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            PlaylistViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = PlaylistVM;
+            });
         }
     }
 }
