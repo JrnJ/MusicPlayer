72646eba7526a2bed0f0655981367a16fe62fc24
JrnJ <jeroenjochems@outlook.com>
2022-04-03 22:04:02 +0200

    Moving to MVVM

---
+++ MusicPlayer/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MusicPlayer.Core;
+
+namespace MusicPlayer.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public PlaylistViewModel PlaylistVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            PlaylistVM = new PlaylistViewModel();
+        }
+    }
+}
---
+++ MusicPlayer/MVVM/ViewModel/PlaylistViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.MVVM.ViewModel
+{
+    class PlaylistViewModel
+    {
+        public PlaylistViewModel()
+        {
+
+        }
+    }
+}
